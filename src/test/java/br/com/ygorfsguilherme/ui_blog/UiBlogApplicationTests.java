32abbcd9a9096fd36551beac5772df97e2da8875
Ygor Guilherme <100237670+ygorfsguilherme@users.noreply.github.com>
2025-02-13 19:55:58 +0000

    Projeto Inicial

---
+++ src/test/java/br/com/ygorfsguilherme/ui_blog/UiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ygorfsguilherme.ui_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
